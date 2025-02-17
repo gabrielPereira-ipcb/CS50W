e98d330f40cfd1bce9dec254ebb35e6dfa33f352
gabrielPereira-ipcb <gsantana99.gs@gmail.com>
2025-02-17 18:17:09 +0000

    Search by URL DONE

--- Wiki/wiki/encyclopedia/views.py
+++ Wiki/wiki/encyclopedia/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    if entry:
+        return render(request, "encyclopedia/entry.html", {
+            "content": markdown2.markdown(entry),
+            "title": title
+        })
+
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
 
